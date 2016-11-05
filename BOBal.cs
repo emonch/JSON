b38f3fcb0bd98f75e4f1fee7fb2b5f781e6289ff
emonch <erich.monch@gmail.com>
2016-11-05 18:16:24 -0200

    Add files via upload

---
+++ BOBal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Bal
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface BOBal
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="/Balanco/{id}")]
+        Balanco GetBalanco(string id);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract(Namespace="")]
+    public class Balanco
+    {
+        string Current_balance;
+
+        [DataMember]
+        public string current_balance
+        {
+            get { return Current_balance; }
+            set { Current_balance = value; }
+        }
+    }
+}
